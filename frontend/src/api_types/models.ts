ce4d05f60c5d58c0091fc0379a037dc3cba0ba25
Lior Hadassi <hadassi@DESKTOP-TUFD7B8.>
2025-02-07 16:15:44 +0200

    switching to manual type manegment

--- frontend/src/api_types/models.ts
+++ frontend/src/api_types/models.ts
@@ -1,10 +1,3 @@
-/* tslint:disable */
-/* eslint-disable */
-/**
-/* This file was automatically generated from pydantic models by running pydantic2ts.
-/* Do not modify it by hand - just update the pydantic models and then re-run the script
-*/
-
 export type EventStatus = "pending" | "ongoing" | "completed";
 
 export interface Event {
@@ -14,11 +7,4 @@ export interface Event {
   end: string | null;
   status?: EventStatus | null;
 }
-export interface Season {
-  id?: number | null;
-  name: string | null;
-  start: string | null;
-  end: string | null;
-  status?: EventStatus | null;
-}
-export interface SQLModel {}
+export interface Season extends Event {}
